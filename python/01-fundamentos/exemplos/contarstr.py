1ca7a6e5fddc7a94882550a66157a88bdaa612a7
Lucas Oliveira Barbosa <lucas.oli.barb@gmail.com>
2026-07-07 13:29:49 -0300

    algumas anotações rápidas sobre o curso

---
+++ python/01-fundamentos/exemplos/contarstr.py
@@ -0,0 +1,1 @@
+print(len("Lucas")) #Função usada pra contar o tamanho da string, bom para fazer exercícios sobre isso!!!
---
+++ python/01-fundamentos/exemplos/varia.py
@@ -0,0 +1,6 @@
+print(3.14 * 5 ** 2) #Função usada pra calcular a área do círculo
+
+pi = 3.14
+raio = 5
+area = pi * raio ** 2
+print(area) #Utilização de variáveis um pouco mais "quebrado" pra ser um passo a passo
